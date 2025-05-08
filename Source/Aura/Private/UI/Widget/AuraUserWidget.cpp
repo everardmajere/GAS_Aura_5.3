015e8bc95451fbd03dc5dbbe2c62ea7c8f09653b
everardmajere <77295078+everardmajere@users.noreply.github.com>
2025-05-07 17:42:05 -1000

    Aura User Widget and Widget Controller

---
+++ Source/Aura/Private/UI/Widget/AuraUserWidget.cpp
@@ -0,0 +1,13 @@
+// Copyright S.R.Hamilton.Art
+
+
+#include "UI/Widget/AuraUserWidget.h"
+
+#include "UI/WidgetController/AuraWidgetController.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController;
+
+	WidgetControllerSet();
+}
---
+++ Source/Aura/Public/UI/Widget/AuraUserWidget.h
@@ -0,0 +1,31 @@
+// Copyright S.R.Hamilton.Art
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "AuraUserWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAuraUserWidget : public UUserWidget
+{
+	GENERATED_BODY()
+public:
+
+	UFUNCTION(blueprintCallable)
+	void SetWidgetController(UObject* InWidgetController);
+	
+	UPROPERTY(BlueprintReadOnly)
+	TObjectPtr<UObject> WidgetController;
+
+protected:
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void WidgetControllerSet();
+	
+	
+	
+};
