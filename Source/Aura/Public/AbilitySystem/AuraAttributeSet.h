eed6428733b17d64a2d8e42562062c1e2abce8a8
everardmajere <77295078+everardmajere@users.noreply.github.com>
2025-05-07 16:25:10 -1000

    Attribute Accessors

--- Source/Aura/Public/AbilitySystem/AuraAttributeSet.h
+++ Source/Aura/Public/AbilitySystem/AuraAttributeSet.h
@@ -4,7 +4,16 @@
 
 #include "CoreMinimal.h"
 #include "AttributeSet.h"
+#include "AbilitySystemComponent.h"
 #include "AuraAttributeSet.generated.h"
+
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+	GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
+  
+
 
 /**
  * 
@@ -29,6 +38,10 @@ public:
 	void OnRep_Health(const FGameplayAttributeData& OldHealth) const;
 	UFUNCTION()
 	void OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const;
+	ATTRIBUTE_ACCESSORS(UAuraAttributeSet, Health);
+	ATTRIBUTE_ACCESSORS(UAuraAttributeSet, MaxHealth);
+	//UFUNCTION()
+	//void OnRep_Health(const FGameplayAttributeData& OldHealth) const;
 	//End Health
 
 	//Mana
@@ -41,6 +54,8 @@ public:
 	void OnRep_Mana(const FGameplayAttributeData& OldMana) const;
 	UFUNCTION()
 	void OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana) const;
+	ATTRIBUTE_ACCESSORS(UAuraAttributeSet,Mana);
+	ATTRIBUTE_ACCESSORS(UAuraAttributeSet,MaxMana);
 	//End Mana
 
 	
